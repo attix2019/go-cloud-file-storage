4e6141ae44021af5d58bc9d7a35031f3e71e2abd
chengr25 <384372602@qq.com>
2021-12-22 14:34:30 +0800

    2-1.上传文件接口实现

--- handler/handler.go
+++ handler/handler.go
@@ -2,18 +2,45 @@ package handler
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 )
+
+func UploadSuccessHandler(w http.ResponseWriter, r* http.Request){
+	io.WriteString(w, "upload succeeded")
+}
 
 func UploadHandler(w http.ResponseWriter, r* http.Request){
 	if r.Method =="GET" {
 		data, err := ioutil.ReadFile("./static/view/index.html")
 		if err != nil{
-			fmt.Printf(err.Error())
+			fmt.Printf("error occured when reading index.html : %s\n" + err.Error())
 		}
 		w.Write(data)
 	}else if r.Method =="POST"{
-		//
+
+		receivedFile, handler, err := r.FormFile("file")
+		if err != nil {
+			fmt.Printf("error occured during uploading file: %s\n", err.Error())
+		}
+		defer receivedFile.Close()
+
+		localFilePath := filepath.Join("tmp")
+		os.MkdirAll(localFilePath, os.ModePerm)
+		localFile, err := os.OpenFile(filepath.Join(localFilePath, handler.Filename), os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil{
+			fmt.Printf("error occured during opening file: %s\n" + err.Error() )
+		}
+		defer localFile.Close()
+
+		_, err = io.Copy(localFile, receivedFile)
+		if err != nil{
+			fmt.Printf("error occured during saving file : %s\n" + err.Error())
+		}
+
+		http.Redirect(w,r, "/file/upload/success", http.StatusFound)
 	}
 }
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import "filestore-server/handler"
 
 func main(){
 	http.HandleFunc("/file/upload", handler.UploadHandler )
+	http.HandleFunc("/file/upload/success", handler.UploadSuccessHandler)
+
 	err:= http.ListenAndServe(":8080", nil)
 	if err!= nil{
 		fmt.Print(err.Error())
